e36776f8d0280daaec08119156609d775d924520
4uwebsite <ramee.nana@gmail.com>
2025-01-10 09:46:20 +0530

    1.0.0
    Prototype code base.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ramith
  My website.
+
+## Versioning 
+- 1.0.0:
+First production version. Migrated prototype code.
---
+++ main.js
@@ -0,0 +1,22 @@
+// Contact buttons
+const number = '+94702338773'
+function call() {
+    window.open(`tel:${number}`)
+}
+function message() {
+    window.open(`sms:${number}`)
+}
+function whatsapp() {
+    window.open(`https://wa.me/${number}`)
+}
+function email() {
+    window.open('mailto:ramee.nana@gmail.com')  
+}
+
+// Experience years
+let yearsSpan = document.getElementById('years')
+const years = new Date().getFullYear() - 2002
+yearsSpan.textContent = years
+
+// Copyright year
+let copyrightYearSpan = document.getElementById('copyright-year').textContent = new Date().getFullYear()
